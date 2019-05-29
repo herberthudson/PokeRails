d3140b363d73b38e7339de8b3dc0c93e1cfd1cb0
Herbert <herbert.trindade@businessintegrator.com.br>
2019-05-29 14:42:57 -0300

    removendo method view

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,20 +1,4 @@
 class CitiesController < ApplicationController
   def index
   end
-
-  def view
-    # get city name
-    @city = params[:city]
-    # url for weather
-    url = "http://api.openweathermap.org/data/2.5/weather"
-    
-    response = Faraday.get do |req| 
-      req.url url , :q => @city
-      req.params['APPID'] = ENV['APPID']
-      req.params['units'] = 'metric'
-    end
-
-    @temp = JSON.parse response.body
-
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pokemons/view'
   get 'cities/index'
-  get 'cities/view'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
