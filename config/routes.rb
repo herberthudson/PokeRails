c03229f9f77f1e6daf245c8f0025d76d60788016
Herbert Hudson <herberthudson@gmail.com>
2019-05-29 22:29:05 -0300

    atualizando rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'pokemons/view'
-  get 'cities', to: 'cities#index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
